0ac545165de899fd8de812e2582949443e43433a
sidhant-ally <sidhant@ally.io>
2021-07-01 20:33:13 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Topbar from './components/Topbar'
 import LatestPostSection from './components/LatestPostSection'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Topbar />
       <LatestPostSection />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import "./index.css"
+
+const Footer = () => {
+    return (<footer>
+        <div>
+            <p class="footer-heading">Edyoda</p>
+            <p class="footer-link">About Us</p>
+            <p class="footer-link">Contact Us</p>
+            <p class="footer-link">Terms of Use</p>
+            <p class="footer-link">Privacy Policy</p>
+        </div>
+        <div>
+            <p class="footer-heading">Resources</p>
+            <p class="footer-link">Courses</p>
+            <p class="footer-link">Learning Videos</p>
+            <p class="footer-link">Educators</p>
+            <p class="footer-link">EdYoda Stories</p>
+            <p class="footer-link">University</p>
+        </div>
+        <div>
+            <p class="footer-heading">For Enterprises</p>
+            <p class="footer-link">Train Your Employees</p>
+            <p class="footer-heading">Quick Links</p>
+            <p class="footer-link">Learn and Earn</p>
+            <p class="footer-link">Become a Learning-Enabler</p>
+            <p class="footer-link">Tips for Learning-Enabler</p>
+            <p class="footer-link">Request New Topic</p>
+        </div>
+        <div>
+            <p class="footer-heading">Connect</p>
+            <p class="footer-link">Building 101</p>
+            <p class="footer-link">Central Avenue</p>
+            <p class="footer-link">LA - 902722</p>
+            <p class="footer-link">United States</p>
+        </div>
+    </footer>);
+}
+
+export default Footer;
